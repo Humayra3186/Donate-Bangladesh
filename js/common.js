02fd0cbb65a1856491dd0fbfa21112c816578967
Humayra <humayra@shaikhnoMacBook-Pro.local>
2024-09-23 04:11:47 +0600

    all the donate buttons are fully interactive

---
+++ js/common.js
@@ -0,0 +1,24 @@
+//to get inner text
+
+function getInnerText (id){
+    const innerText = document.getElementById(id).innerText;
+    const number = parseFloat(innerText);
+    return number;
+}
+
+//to get input value
+
+
+
+function getInputValue(id){
+    const inputValue = document.getElementById(id).value;
+    const number = parseFloat(inputValue);
+
+    return number;
+};
+
+// history create
+
+
+
+
--- js/home.js
+++ js/home.js
@@ -4,3 +4,96 @@ document.getElementById('blog').addEventListener('click' , function(){
     window.location.href = './blog.html'
 })
 
+
+
+
+  
+
+//when click on the donate now button
+
+// card one
+document.getElementById('btn-one').addEventListener('click' , function(){
+    let balance = getInnerText('balance')
+    const amount = getInputValue('amount-one');
+    let donate = getInnerText('donate');
+   
+    
+    if(Math.sign(amount) !== -1 && isNaN(amount) === false){
+       
+        const newDonate = amount + donate;
+        document.getElementById('donate').innerText = newDonate;
+
+        const remainBalance = balance - amount;
+        document.getElementById('balance').innerText = remainBalance;
+       
+
+    }
+    else{
+        alert('Invalid Donation Amount!');
+        return;
+    }
+
+
+     my_modal_1.showModal();
+     
+})
+
+
+//card 2
+
+document.getElementById('btn-two').addEventListener('click' , function(){
+    let balance = getInnerText('balance')
+    const amount = getInputValue('amount-two');
+    let donate = getInnerText('donate-two');
+   
+    
+    if(Math.sign(amount) !== -1 && isNaN(amount) === false){
+       
+        const newDonate = amount + donate;
+        document.getElementById('donate-two').innerText = newDonate;
+
+        const remainBalance = balance - amount;
+        document.getElementById('balance').innerText = remainBalance;
+       
+
+    }
+    else{
+        alert('Invalid Donation Amount!');
+        return;
+    }
+
+
+     my_modal_1.showModal();
+     
+})
+
+
+
+//card 
+
+document.getElementById('btn-three').addEventListener('click' , function(){
+    let balance = getInnerText('balance')
+    const amount = getInputValue('amount-three');
+    let donate = getInnerText('donate-three');
+   
+    
+    if(Math.sign(amount) !== -1 && isNaN(amount) === false){
+       
+        const newDonate = amount + donate;
+        document.getElementById('donate-three').innerText = newDonate;
+
+        const remainBalance = balance - amount;
+        document.getElementById('balance').innerText = remainBalance;
+       
+
+    }
+    else{
+        alert('Invalid Donation Amount!');
+        return;
+    }
+
+
+     my_modal_1.showModal();
+     
+})
+
